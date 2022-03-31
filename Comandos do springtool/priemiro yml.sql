a90669c9360e3daa59376f95dd753d1dcaf80e5c
Danilo <danilo.chierici@gmail.com>
2022-03-31 17:03:03 -0300

    commit2.

---
+++ Comandos do springtool/priemiro yml.sql
@@ -0,0 +1,13 @@
+server:
+   port: 8000
+spring:
+ application:
+  name: cambio-service
+  
+package br.com.eurudio.model;
+
+public class cambio {
+	private long id;
+	private string from;
+	private string from;
+} 
